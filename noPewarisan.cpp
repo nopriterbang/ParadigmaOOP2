e9b787964c2f8be9b3a483d05503ce2c4bd62780
nopriterbang <novalpriya@gmail.com>
2024-05-27 14:57:49 +0700

    menambahkan class baseClass dengan menghentikan pewarisan

---
+++ noPewarisan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final{
+public:
+    virtual void perkenalan(){
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+class deriveredClass
